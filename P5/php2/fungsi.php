7ead900d122dc44db094c46c35db8663e12210cf
BillyMaulanaFerdinan <billy.maulana2020@gmail.com>
2024-10-03 19:49:40 +0700

    P5 Praktikum php2 No 5

--- P5/php2/fungsi.php
+++ P5/php2/fungsi.php
@@ -5,5 +5,15 @@ function hitungUmur($thn_lahir, $thn_sekarang) {
     return $umur;
 }
 
-echo "Umur saya adalah ". hitungUmur(2003, 2024) ." tahun"
+function perkenalan($nama, $salam="Assalamualaikum") {
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+
+    // Memanggil fungsi lain
+    echo "Saya berusia ". hitungUmur(2003, 2024) ." tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+// Memanggil fungsi perkenalan
+perkenalan("Billy");
 ?>
