7220310ee6efaa5ccf1ba258ba31c1bea885827a
BillyMaulanaFerdinan <billy.maulana2020@gmail.com>
2024-10-03 17:40:38 +0700

    P5 Praktikum php2 No 1

---
+++ P5/php2/fungsi.php
@@ -0,0 +1,12 @@
+<?php
+
+function perkenalan() {
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Billy<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+// Memanggil fungsi ynag dibuat
+perkenalan();
+
+?>
