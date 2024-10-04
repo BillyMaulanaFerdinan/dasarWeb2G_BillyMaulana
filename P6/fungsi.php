2b5659b443c1bf92cbea35d452606ef734d38cb7
BillyMaulanaFerdinan <billy.maulana2020@gmail.com>
2024-10-04 09:07:01 +0700

    P6 Praktikum 1 No 4

---
+++ P6/fungsi.php
@@ -0,0 +1,12 @@
+<?php
+
+function perkenalan() {
+    echo "Assalamualaikum";
+    echo "Perkenalkan, nama saya Billy<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+// Memanggil fungsi yang sudah dibuat
+perkenalan();
+perkenalan();
+?>
